058b0e494a00f0988bcd99ca557151d8678e6860
ssmeds <stina_smeds@hotmail.com>
2021-02-10 11:41:36 +0100

    added repo api to my portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+// <!--------------- Toggle menu/navbar script ----------->
+// This script toggles the class .active on the id #nav when it's clicked
+function myFunction() {
+    var mobileMenu = document.getElementById("nav");
+    mobileMenu.classList.toggle("active");
+}
+
+fetch("https://api.github.com/users/ssmeds/repos")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    let div = document.getElementById("repos");
+    for (repo in data) {
+        div.insertAdjacentHTML("afterbegin", `<div> <a href='${data[repo].html_url}' target='_blank'>${data[repo].name}</a></div>`);
+    }
+})
